6cad00d30de76a58b49b293d74e93a0298842bd8
Louis-Philippe0 <louis-philippe42lpp@hotmail.com>
2023-03-25 18:53:40 -0400

    movement stuff

--- doStuff.js
+++ doStuff.js
@@ -1,4 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
     let square = document.getElementById('square');
-    console.log(square);
 });
+
+const keyMap = {
+    'w': 'up',
+    'a': 'left',
+    's': 'down',
+    'd': 'right'
+};
+
+document.addEventListener('keydown', (event) => {
+    const direction = keyMap[event.key];
+    if (direction) {
+        move(square, direction, 5);
+    }
+});
+
+function move(target, direction, distance) {
+    if (!(target instanceof HTMLElement)) {
+        console.error("Target is not an HTML element", target);
+        return;
+    }
+    if (!["up", "down", "left", "right"].includes(direction)) {
+        console.error("Invalid direction", direction);
+        return;
+    }
+    if (!(typeof distance === "number")) {
+        console.error("Distance is not a number", distance);
+        return;
+    }
+    const directionMap = {
+        up: 'top',
+        down: 'top',
+        left: 'left',
+        right: 'left'
+    };
+    let currentPos = target.getBoundingClientRect()[directionMap[direction]];
+
+    setInterval(() => {
+        currentPos += distance;
+        target.style[directionMap[direction]] = currentPos + 'px';
+    }, 50);
+
+};
